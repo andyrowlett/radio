8880538d31e05c2ca479bbb245f4bfd5fdeda72b
andyrowlett <lodjer@gmail.com>
2023-12-26 12:42:03 +0000

    Create player.py

---
+++ player.py
@@ -0,0 +1,65 @@
+import i2c_dev as drivers
+from time import sleep
+from datetime import datetime
+from rotary_class import RotaryEncoder
+import RPi.GPIO as GPIO
+import time
+
+display = drivers.Lcd()
+display.lcd_backlight(1)
+cc = drivers.CustomCharacters(display)
+cc.char_1_data = ["11111",
+                "11111",
+                "11111",
+                "11111",
+                "11111",
+                "11111",
+                "11111",
+                "11111"]
+cc.load_custom_characters_data()
+
+def indicate(text):
+    display.lcd_display_string(text, 1)
+
+display("Hi")
+
+
+station = 0
+def rotary_unit_callback(event):
+    global station
+    if event == RotaryEncoder.CLOCKWISE:
+        if station < max:
+            station += 1
+    elif event == RotaryEncoder.ANTICLOCKWISE:
+        if station > min:
+            station -= 1
+    indicate(station)
+    print("S=%s" % station)
+    #time.sleep(bb)
+
+# Define GPIO inputs for rotary encoder
+PIN_A = 18 	
+PIN_B = 17	
+BUTTON = 0
+rswitch = RotaryEncoder(PIN_A,PIN_B,BUTTON,rotary_unit_callback)
+
+volume = 50
+def volume_callback(event):
+    global volume
+    if event == RotaryEncoder.CLOCKWISE:
+        if volume < 100:
+            volume += 10
+    elif event == RotaryEncoder.ANTICLOCKWISE:
+        if volume > 0:
+            volume -= 10
+    indicate(station)
+    print("S=%s" % station)
+    #time.sleep(bb)
+
+GPIO.setup(20, GPIO.OUT)
+GPIO.output(20, GPIO.LOW)
+# Define GPIO inputs for volume
+PIN_VA = 19	
+PIN_VB = 21
+BUTTON = 0
+vol = RotaryEncoder(PIN_VA,PIN_VB,BUTTON,volume_callback)
