cbafbd3a478fd3c94e7ec8657bdf4190cf2261a5
andyrowlett <lodjer@gmail.com>
2023-12-31 11:52:48 +0000

    Update cls_mpc.py

--- cls_mpc.py
+++ cls_mpc.py
@@ -3,7 +3,7 @@ import os
 class Playlist:
 	def reinit(self):
 		pls = os.popen("mpc playlist").read()
-		self.playlist = pls.splitlines
+		self.playlist = pls.splitlines()
 		self.playlist_length = len(self.playlist)
 
 	def __init__(self):
